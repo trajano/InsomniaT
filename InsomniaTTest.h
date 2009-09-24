311cc8a1d6df64d117de1473a01f9e8e804ae4dd
Archimedes Trajano <developer@trajano.net>
2009-09-24 18:44:53 -0400

    Unit tests all run successfully
    original bzr status:
    revno: 32
    committer: Archimedes Trajano <developer@trajano.net>
    branch nick: InsomniaT
    timestamp: Thu 2009-09-24 18:44:53 -0400

---
+++ InsomniaTTest.h
@@ -0,0 +1,16 @@
+//
+//  InsomniaTTest.h
+//  InsomniaT
+//
+//  Created by Archimedes Trajano on 2009-09-24.
+//  Copyright 2009 trajano.net. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+
+@interface InsomniaTTest : SenTestCase {
+
+}
+
+@end
