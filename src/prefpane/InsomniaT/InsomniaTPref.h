29a2ccf9ec2aa248618f6d145a4c1cb03a20b886
Archimedes Trajano <developer@trajano.net>
2010-06-07 19:54:45 -0400

    initial base for prefpane. Also added some space to the text to make it look better.
    original bzr status:
    revno: 54
    committer: Archimedes Trajano <developer@trajano.net>
    branch nick: InsomniaT
    timestamp: Mon 2010-06-07 19:54:45 -0400

---
+++ src/prefpane/InsomniaT/InsomniaTPref.h
@@ -0,0 +1,19 @@
+//
+//  InsomniaTPref.h
+//  InsomniaT
+//
+//  Created by Archimedes Trajano on 2010-06-07.
+//  Copyright (c) 2010 trajano.net. All rights reserved.
+//
+
+#import <PreferencePanes/PreferencePanes.h>
+
+
+@interface InsomniaTPref : NSPreferencePane 
+{
+
+}
+
+- (void) mainViewDidLoad;
+
+@end
---
+++ src/prefpane/InsomniaT/InsomniaTPref.m
@@ -0,0 +1,18 @@
+//
+//  InsomniaTPref.m
+//  InsomniaT
+//
+//  Created by Archimedes Trajano on 2010-06-07.
+//  Copyright (c) 2010 trajano.net. All rights reserved.
+//
+
+#import "InsomniaTPref.h"
+
+
+@implementation InsomniaTPref
+
+- (void) mainViewDidLoad
+{
+}
+
+@end
